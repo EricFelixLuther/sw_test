f80727b147bf9e4847fef262635ab2e63385de46
Eric Luther <ericfelixluther@gmail.com>
2017-12-11 20:34:43 +0100

    red black tree

--- algorithms/src/AVL/app.java
+++ algorithms/src/AVL/app.java
@@ -14,5 +14,4 @@ public class app {
 		avl.delete(5);
 		avl.traverse();
 	}
-
 }
---
+++ algorithms/src/RedBlackTree/App.java
@@ -0,0 +1,14 @@
+package RedBlackTree;
+
+public class App {
+
+	public static void main(String[] args) {
+		Tree rbt = new RedBlackTree();
+		rbt.insert(10);
+		rbt.insert(16);
+		rbt.insert(13);
+		
+
+	}
+
+}
